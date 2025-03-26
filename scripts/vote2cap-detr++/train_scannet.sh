92c1b6b6dc6970fdb2dd3a80878235b84f627c75
YFMika <143109264+YFMika@users.noreply.github.com>
2025-03-26 16:34:40 +0800

    Update train_scannet.sh

--- scripts/vote2cap-detr++/train_scannet.sh
+++ scripts/vote2cap-detr++/train_scannet.sh
@@ -2,7 +2,4 @@ python main.py \
     --use_color \
     --use_normal \
     --detector detector_Vote2Cap_DETRv2 \
-    --checkpoint_dir pretrained/Vote2Cap_DETRv2_XYZ_COLOR_NORMAL \
-    --gpu 3 \
-    --max_epoch 1080 \
-    --batchsize_per_gpu 8
+    --checkpoint_dir pretrained/Vote2Cap_DETRv2_XYZ_COLOR_NORMAL
